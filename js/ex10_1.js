226d63932d3a2fab559152f40d0719645f8113b0
Roberto Borba <r.robertoborba@gmail.com>
2026-06-25 19:58:14 -0300

    ajustes na função do click

--- js/ex10_1.js
+++ js/ex10_1.js
@@ -12,14 +12,14 @@ frm.addEventListener("submit", (e) => {
     dvQuadro.appendChild(h5) // E que h5 será filho de dvQuadro
     
     frm.inTarefa.value = "" // Limpa o campo de edição
-    frm.inTarefa.focus()    // Joga o cursor nesse campo
+    frm.inTarefa.focus()   // Joga o cursor nesse campo
 })
 
 frm.addEventListener("click", () => {
 
-    const tarefas = document.querySelectorAll("h5") // Obtém as tags h5 da página
+    const tarefas = document.querySelectorAll("h5") // Obtém as tags h5 da página queryselectorALL retorna um vetor
 
-    if (tarefas.length == 0)
+    if (tarefas.length == 0) 
         alert("não há tarefas para selecionar") // se não houver tarefas, exibe os alertas
     return // e retorna
 
@@ -30,10 +30,17 @@ frm.addEventListener("click", () => {
     for (let i = 0; i < tarefas.length; i++) { 
         // se tag é da class tarefa-selecionada (está selecionada)
         if (tarefas[i].className == "tarefa selecionada") {
-            
+            tarefas[i].className = "tarefa-normal" // troca para normal
+            aux = i // Muda valor da variável auxiliar
+            break // Sai da repetição 
         }
 
     }
 
+    // Se a linha que está selecionada é a última, irá voltar para a primeira
+    if (aux == tarefas.length -1) {
+        aux = -1
+    }
 
+    tarefas[aux + 1].className = "tarefa-selecionada" // muda estilo da próxima linha 
 })
