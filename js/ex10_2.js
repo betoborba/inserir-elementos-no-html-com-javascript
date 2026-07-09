e6c640b48f76a180023de719c7c78e0f439b09b7
Roberto Borba <r.robertoborba@gmail.com>
2026-07-09 19:05:50 -0300

    finalização jogo

--- js/ex10_2.js
+++ js/ex10_2.js
@@ -24,11 +24,11 @@ window.addEventListener("load", () => {
 
 })
 
-const criarMoedas = (num, moeda, textoALt, classe) => {
+const criarMoedas = (num, moeda, textoALt, classe) => { // Argumentos passados na ordem da chamada da função
 
     //Cria o laço de repetição para inserir várias imagens de moedas na página
 
-    for (let i = 1; i <= num; i++) {
+    for (let i = 1; i <= num; i++) { 
         const novaMoeda = document.createElement("img") // Cria o elemento img
         novaMoeda.src = "img/" + moeda // Atributo src
         novaMoeda.textoALt = textoALt // Texto alternativo
@@ -40,3 +40,52 @@ const criarMoedas = (num, moeda, textoALt, classe) => {
     dvMoedas.appendChild(br)
 
 }
+
+frm.addEventListener("submit",(e) => {
+    e.preventDefault(); // Evita o envio do form
+
+    const soma = Number(frm.inSoma.value) // Obtém o valor do usuário
+    const moedas = dvMoedas.querySelectorAll("img") // Obtém img filhas de dvMoedas
+    let totalMoedas = 0 // Declara e inicializa o contador
+
+    // Percorre as tags img (em moedas) e verifica propriedade className
+    for (const moeda of moedas) {
+        if (moeda.className == "moeda1-00") {
+            totalMoedas += 1 // Acumula 1 (para moedas de 1)
+        } else if (moeda.className == "moeda0-50") {
+            totalMoedas += 0.5  // Acumula 0.50 (para moedas de 0.50)
+        } else if (moeda.className == "moeda0-25") {
+            totalMoedas += 0.25 // Acumula 0.25 (para moedas de 0.25)
+        } else {
+            totalMoedas += 0.1 // Acumula 0.1 (para moedas de 0.10)
+        }
+    }
+
+    const div = document.createElement("div") // Cria o elemento div 
+    const h3 = document.createElement("h3") // Cria o elemento h3
+
+    let mensagem // let porque a mensagem muda não existe um valor definido
+
+    // Verifica se o valor inserido é igual ao total de Moedas exibido 
+
+    if (soma == totalMoedas.toFixed(2)) {
+        div.className = "alert alert-success" // Define a classe da div
+        mensagem = "Parabéns! Você acertou!" // e mensagem a ser exibida
+    } else {
+        div.className = "alert alert-danger"
+        mensagem = `Ops.. A resposta correta é ${totalMoedas.toFixed(2)}`
+    }
+
+    const texto = document.createTextNode(mensagem) // Cria elemento de texto
+
+    h3.appendChild(texto) // Texto é filho de h3
+    div.appendChild(h3) // h3 é filho da vida criada na function
+    dvMoedas.appendChild(div) // Div com alerta é filha de dvMoedas
+
+    frm.submit.disabled = true // Desabilita o botão (resposta já foi exibida)
+})
+
+frm.addEventListener("reset", () => {
+    window.location.reload()
+})
+
